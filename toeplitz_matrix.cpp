e00bd2a9228ac3c1aecdcbc5c1ab4372ed9db159
qqweng <qqweng@ucdavis.edu>
2019-03-18 23:45:37 -0700

    Add files via upload

---
+++ toeplitz_matrix.cpp
@@ -0,0 +1,32 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+using namespace std;
+// [[Rcpp::export]]
+NumericMatrix toeplitz_matrix(int n, NumericVector x) {
+  if (n < 2)
+    stop( "argument n is less than 2" );
+  if(!Rf_isVector(x)) {
+    stop( "argument x is not a vector" );
+  }
+  if (x.length() != (2 * n - 1))
+    stop( "length of argument x is not comparable with n" );
+  
+  NumericMatrix T(n);
+  
+  for (int i = 0; i < n; ++i) 
+    for (int j = 0; j < n; ++j)
+      T(i,j) = x((i - j) + (n - 1));
+  
+  return T;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+
+*/
